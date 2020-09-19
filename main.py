c18a606051ace6caa5617f4d11be1f878abf395d
Samuel Adekunle <me@samuel-adekunle.software>
2020-09-19 14:50:11 +0100

    fix: fixes marquee processing

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ try:
 except ValueError:
     gov_frame = gov_api.get_dataframe()
     gov_frame.to_json("data/gov.json")
+    gov_frame.to_csv("data/gov.csv")
 
 try:
     marquee_frame = pd.read_json("data/marquee.json")
 except ValueError:
-    pass
     marquee_frame = marquee_api.get_data(
         countryId='GB', start=date(2020, 1, 3), fields=["ccgCode", "count"])
     marquee_frame = marquee_frame.drop("countryId", axis=1)
-    # marquee_frame.to_csv("data/marquee.csv")
+    marquee_frame = marquee_frame.groupby(
+        ["date", "ccgCode"]).aggregate({"count": "sum"})
+    marquee_frame.to_json("data/marquee.json")
+    marquee_frame.to_csv("data/marquee.csv")
