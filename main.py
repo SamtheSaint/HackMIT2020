f8a05d1b85e60c2a6d4586f98856a6dbbf0a4cc8
nd419 <5161147+neeldug@users.noreply.github.com>
2020-09-19 14:08:06 +0100

    fix: change exception error to ValueError
    Signed-off-by: nd419 <5161147+neeldug@users.noreply.github.com>

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import pandas as pd
 
 # pull the UK data into a Pandas dataframe
 try:
-    gov_frame = pd.read_csv("data/gov.csv")
-except OSError:
+    gov_frame = pd.read_json("data/gov.json")
+except ValueError:
     gov_frame = gov_api.get_dataframe()
-    gov_frame.to_csv("data/gov.csv")
+    gov_frame.to_json("data/gov.json")
 
 try:
-    marquee_frame = pd.read_csv("data/marquee.csv")
-except OSError:
+    marquee_frame = pd.read_json("data/marquee.json")
+except ValueError:
     pass
     marquee_frame = marquee_api.get_data(
         countryId='GB', start=date(2020, 1, 3))
