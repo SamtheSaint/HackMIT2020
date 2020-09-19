f829d08755546d1fa7090c9042cbf1cc4723a948
Samuel Adekunle <me@samuel-adekunle.software>
2020-09-19 21:47:56 +0100

    feat: start time series analysis
    Signed-off-by: Samuel Adekunle <me@samuel-adekunle.software>

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from datetime import date
 from settings import marquee_api, gov_api
 import pandas as pd
+import gs_quant.timeseries as ts
 
 # pull the UK data into a Pandas dataframe
 try:
-    gov_frame = pd.read_csv("data/gov.csv", index_col="date")
+    gov_frame = pd.read_csv("data/gov.csv")
+    gov_frame['date'].apply(lambda x: date.fromisoformat(x))
+    gov_frame.set_index("date", inplace=True)
 except IOError:
     gov_frame = gov_api.get_dataframe()
     gov_frame.set_index("date", inplace=True)
@@ -12,6 +15,7 @@ except IOError:
 
 try:
     marquee_frame = pd.read_csv("data/marquee.csv")
+    marquee_frame['date'].apply(lambda x: date.fromisoformat(x))
 except IOError:
     marquee_frame = marquee_api.get_data(
         countryId='GB', start=date(2020, 1, 3), fields=["ccgCode", "count"])
@@ -30,8 +34,14 @@ ts_list = []
 for coord in coords:
     ts_cases = gov_frame[gov_frame['areaCode'] == coord[1].strip()]
     ts_phones = marquee_frame[marquee_frame['ccgCode'] == coord[0].strip()]
-    ts_link = (ts_phones, ts_cases)
+    ts_link = (ts_phones.drop(columns=["ccgCode"]),
+               ts_cases.drop(columns=["areaCode"]))
+    # if phones or cases < n
     ts_list.append(ts_link)
 
-# print(ts_list)
 # TODO: Perform time-series analysis to both to find lag correlation!
+for pair in ts_list:
+    # print(pair[0]["count"])
+    # print(pair[1]["newCasesByPublishDate"])
+    print(ts.econometrics.autocorrelation(
+        pair[0]["count"], pair[1]["newCasesByPublishDate"], 10))
