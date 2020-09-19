2e5df19bdad01ab12a3708cebfb64d51ce5f581f
Samuel Adekunle <me@samuel-adekunle.software>
2020-09-19 14:35:11 +0100

    feat: clean up marquee data

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ import pandas as pd
 try:
     gov_frame = pd.read_csv("data/gov.csv")
 except:
+    print("start fetching remote gov data")
     gov_frame = gov_api.get_dataframe()
+    print("done fetching remote gov data")
     gov_frame.to_csv("data/gov.csv")
 
 try:
     marquee_frame = pd.read_csv("data/marquee.csv")
 except:
-    pass
     marquee_frame = marquee_api.get_data(
-        countryId='GB', start=date(2020, 1, 3))
-    # TODO - process marquee data in required format
+        countryId='GB', start=date(2020, 1, 3), fields=["ccgCode", "count"])
+    marquee_frame = marquee_frame.drop("countryId", axis=1)
     # marquee_frame.to_csv("data/marquee.csv")
