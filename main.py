9f7dccb3c32f61e7383b240ad0c08e6ca3dc4949
nd419 <5161147+neeldug@users.noreply.github.com>
2020-09-19 19:43:39 +0100

    fix: fix clobber of index
    fix by using `reset_index()` to preserve dataframe. also generates
    linked timeseries
    
    Signed-off-by: nd419 <5161147+neeldug@users.noreply.github.com>

--- main.py
+++ main.py
@@ -17,6 +17,19 @@ except ValueError:
         countryId='GB', start=date(2020, 1, 3), fields=["ccgCode", "count"])
     marquee_frame = marquee_frame.drop("countryId", axis=1)
     marquee_frame = marquee_frame.groupby(
-        ["date", "ccgCode"]).aggregate({"count": "sum"})
+        ["date", "ccgCode"]).aggregate({"count": "sum"}).reset_index()
     marquee_frame.to_json("data/marquee.json")
     marquee_frame.to_csv("data/marquee.csv")
+
+df = pd.read_csv('data/link_table.csv', delimiter=',')
+coords = [list(row) for row in df.values]
+ts_list = []
+# Generates time-series for both datasets
+for coord in coords:
+    ts_cases = gov_frame[gov_frame['areaCode'] == coord[1].strip()]
+    ts_phones = marquee_frame[marquee_frame['ccgCode'] == coord[0].strip()]
+    ts_link = (ts_phones, ts_cases)
+    ts_list.append(ts_link)
+
+# print(ts_list)
+# TODO: Perform time-series analysis to both to find lag correlation!
