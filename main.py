2942e58a2cab45020ae988173beadddce9fff3b3
Samuel Adekunle <me@samuel-adekunle.software>
2020-09-19 20:48:07 +0100

    fix: remove default index from gov frame
    Signed-off-by: Samuel Adekunle <me@samuel-adekunle.software>

--- main.py
+++ main.py
@@ -4,26 +4,28 @@ import pandas as pd
 
 # pull the UK data into a Pandas dataframe
 try:
-    gov_frame = pd.read_json("data/gov.json")
-except ValueError:
+    gov_frame = pd.read_csv("data/gov.csv", index_col="date")
+except IOError:
     gov_frame = gov_api.get_dataframe()
-    gov_frame.to_json("data/gov.json")
+    gov_frame.set_index("date", inplace=True)
     gov_frame.to_csv("data/gov.csv")
 
 try:
-    marquee_frame = pd.read_json("data/marquee.json")
-except ValueError:
+    marquee_frame = pd.read_csv("data/marquee.csv")
+except IOError:
     marquee_frame = marquee_api.get_data(
         countryId='GB', start=date(2020, 1, 3), fields=["ccgCode", "count"])
-    marquee_frame = marquee_frame.drop("countryId", axis=1)
-    marquee_frame = marquee_frame.groupby(
-        ["date", "ccgCode"]).aggregate({"count": "sum"}).reset_index()
-    marquee_frame.to_json("data/marquee.json")
+    marquee_frame = marquee_frame.drop(columns=["countryId"])
     marquee_frame.to_csv("data/marquee.csv")
+finally:
+    marquee_frame = marquee_frame.groupby(["date", "ccgCode"]).aggregate({
+        "count": "sum"}).reset_index()
+    marquee_frame.set_index("date", inplace=True)
 
 df = pd.read_csv('data/link_table.csv', delimiter=',')
 coords = [list(row) for row in df.values]
 ts_list = []
+
 # Generates time-series for both datasets
 for coord in coords:
     ts_cases = gov_frame[gov_frame['areaCode'] == coord[1].strip()]
