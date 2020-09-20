37312415d635fe5becebb7a668f4ebbc9390cc0e
nd419 <5161147+neeldug@users.noreply.github.com>
2020-09-20 14:47:10 +0100

    fix: bug fix using try except hack
    Signed-off-by: nd419 <5161147+neeldug@users.noreply.github.com>

--- main.py
+++ main.py
@@ -43,19 +43,22 @@ for code in codes:
     if len(ts_gov) < tolerance or len(ts_marquee) < tolerance:
         continue
     else:
-        ts_gov_marquee = ts_gov.join(ts_marquee, on="date")
-        ts_gov_marquee['latitude'] = gov_lat_long_link['Latitude'][area_code]
-        ts_gov_marquee['longitude'] = gov_lat_long_link['Longitude'][area_code]
-        ts_gov_marquee['num_of_phone_calls'] = ts_gov_marquee['count']
-        ts_gov_marquee['num_of_new_cases'] = ts_gov_marquee['newCasesBySpecimenDate']
-        ts_gov_marquee = ts_gov_marquee.drop(
-            columns=["areaCode", "ccgCode", "count", "newCasesBySpecimenDate"]).reset_index()
-        marquee_gov_frame = marquee_gov_frame.append(ts_gov_marquee)
+        try: #This is temporary and bypasses duplicate tuple entries, i.e. multiple links to single place
+            ts_gov_marquee = ts_gov.join(ts_marquee, on="date")
+            ts_gov_marquee['latitude'] = gov_lat_long_link.loc[area_code]['Latitude']
+            ts_gov_marquee['longitude'] = gov_lat_long_link.loc[area_code]['Longitude']
+            ts_gov_marquee['num_of_phone_calls'] = ts_gov_marquee['count']
+            ts_gov_marquee['num_of_new_cases'] = ts_gov_marquee['newCasesBySpecimenDate']
+            ts_gov_marquee = ts_gov_marquee.drop(
+                columns=["areaCode", "ccgCode", "count", "newCasesBySpecimenDate"]).reset_index()
+            marquee_gov_frame = marquee_gov_frame.append(ts_gov_marquee)
+        except:
+            continue
 
 marquee_gov_frame.set_index("date", inplace=True)
 marquee_gov_frame.sort_index(inplace=True)
 
-print(marquee_gov_frame)
+marquee_gov_frame.to_csv(f'data.csv')
 
 
 def generate_timeseries():
@@ -70,19 +73,12 @@ def generate_timeseries():
 
     for (ts_marquee, ts_gov) in ts_list:
         area_code = ts_gov["areaCode"][0]
-        # TODO: Calculate optimal correlation lag time for each and do statistical analysis on results, i.e. median, S.D.,
-        #  etc.
-        # TODO: Work out how to find the optimal correlation lag, possibly iterating over windows & mean?
-        # curr_max = -1
-        # for i in range(1,30):
-        #     curr_max = max(ts.econometrics.correlation(
-        #         pair[0]["count"], pair[1]["newCasesBySpecimenDate"], ).mean(), curr_max)
-        # print(f'{curr_max} at lag {i}')
+        curr_max = -1
+        for i in range(1,30):
+            curr_max = max(ts.econometrics.correlation(
+                ts_marquee["count"], ts_gov["newCasesBySpecimenDate"], ).mean(), curr_max)
+        print(f'{curr_max} at lag {i}')
 
-        # print(ts.econometrics.correlation(
-        #     pair[0]["count"], pair[1]["newCasesBySpecimenDate"]))
-        # print(ts.econometrics.correlation(
-        #     ts_marquee["count"], ts_gov["newCasesBySpecimenDate"]))
         # TODO: Trim data to match time for visualisation, shouldn't matter for correlation
         ts_marquee.to_csv(f"exports/marquee{area_code}.csv")
         ts_gov.to_csv(f"exports/gov{area_code}.csv")
--- plot.py
+++ plot.py
@@ -4,7 +4,7 @@ import plotly.graph_objs as go
 
 from plotly.graph_objs import *
 
-df_merged = pd.read_csv("csvfile.csv")  # TODO: REPLACE CSV FILENAME HERE
+df_merged = pd.read_csv("data.csv")  # TODO: REPLACE CSV FILENAME HERE
 
 date = "2020-07-20"
 
@@ -16,15 +16,15 @@ scl = [[0.0, '#ffffff'], [0.2, '#ff9999'], [0.4, '#ff4d4d'],
 for col in df_sect.columns:
     df_sect[col] = df_sect[col].astype(str)
 
-df_sect['text'] = 'Lat: ' + df_sect['Latitude'] + 'Long: ' + df_sect['Longitude'] + 'Phone Calls: ' + \
-                  df_sect['marquee'] + 'Confirmed Cases: ' + df_sect['gov']
+df_sect['text'] = 'Lat: ' + df_sect['latitude'] + 'Long: ' + df_sect['longitude'] + 'Phone Calls: ' + \
+                  df_sect['num_of_phone_calls'] + 'Confirmed Cases: ' + df_sect['num_of_new_cases']
 data = [dict(
     type='choropleth',  # type of map-plot
     colorscale=scl,
     autocolorscale=False,
-    lon=df_sect['Longitude'],
-    lat=df_sect['Latitude'],
-    z=df_sect['Confirmed Cases'].astype(float),
+    lon=df_sect['longitude'],
+    lat=df_sect['latitude'],
+    z=df_sect['num_of_new_cases'].astype(float),
     text=df_sect['text'],  # hover text
     marker=dict(
         line=dict(
@@ -43,6 +43,6 @@ layout = dict(
     ),
 )
 
-fig = dict(data=data, layout=layout)
+fig = go.Figure(data=data, layout=layout)
 
-plotly.offline.iplot(fig)
+fig.show()
